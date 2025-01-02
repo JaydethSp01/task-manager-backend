7c943f276c04fddb4604125f59fc70369385cb6a
JaydethSp01 <jsimarrapolo@gmail.com>
2025-01-02 17:12:46 -0500

    add compression middleware and set cache control headers

--- ServerTaskManager/server.js
+++ ServerTaskManager/server.js
@@ -5,6 +5,7 @@ const swaggerDocument = require("./swagger.json");
 const connectDB = require("./Config/db");
 const taskRoutes = require("./Routes/taskRoutes");
 const cors = require("cors");
+const compression = require("compression");
 
 connectDB();
 
@@ -12,6 +13,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(compression());
+
+app.use((req, res, next) => {
+  res.set("Cache-Control", "public, max-age=60");
+  next();
+});
 
 app.use("/api", taskRoutes);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
@@ -19,7 +26,7 @@ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(
-    `Servidor corriendo con swagger http://localhost:${PORT}/api-docs`
+    `Servidor corriendo con Swagger: http://localhost:${PORT}/api-docs`
   );
 });
 
